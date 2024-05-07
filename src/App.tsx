b2cecae2199f59a540842aa790837f7577545f9e
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-07 16:57:02 +0800

    feat: added tab navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React, {useEffect} from 'react';
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import RegisterScreen from './auth/registerScreen';
 import LoginScreen from './auth/loginScreen';
-import HomeScreen from './screens/homseScreen'; // Typo here, should be 'homeScreen'
+import HomeScreen from './screens/homseScreen';
 import SplashScreen from 'react-native-splash-screen';
-import {Platform, ActivityIndicator, View} from 'react-native';
+import {Platform, ActivityIndicator, View, Text} from 'react-native';
 import {AuthProvider, useAuth} from './hooks/authContext';
+import MIcon from 'react-native-vector-icons/Ionicons';
+
+import ChatScreen from './screens/chatScreen';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   const {token, isLoading} = useAuth();
@@ -31,7 +37,84 @@ const App = () => {
     <NavigationContainer>
       {token ? (
         <Drawer.Navigator>
-          <Drawer.Screen name="Chats" component={HomeScreen} />
+          <Drawer.Screen
+            name="Chats"
+            options={{
+              drawerLabel: 'Chats',
+              title: 'Chats',
+              drawerIcon: () => (
+                <MIcon name="chatbubbles" size={20} color="gray" />
+              ),
+            }}>
+            {() => (
+              <Tab.Navigator
+                screenOptions={{
+                  tabBarShowLabel: true,
+                  headerShown: false,
+                  tabBarHideOnKeyboard: true,
+                }}>
+                <Tab.Screen
+                  name="Chat-tab"
+                  component={HomeScreen}
+                  options={{
+                    tabBarLabel: ({focused}) => (
+                      <Text
+                        className="text-[10px]"
+                        style={{color: focused ? 'red' : 'gray'}}>
+                        Profile
+                      </Text>
+                    ),
+                    headerShown: false,
+                    tabBarIcon: ({color, size, focused}) => (
+                      <MIcon
+                        name={focused ? 'chatbubbles' : 'chatbubbles-outline'}
+                        size={size}
+                        color={color}
+                      />
+                    ),
+                  }}
+                />
+                {/* Add more tab screens here if needed */}
+              </Tab.Navigator>
+            )}
+          </Drawer.Screen>
+
+          <Drawer.Screen
+            name="Chat22"
+            options={{
+              drawerLabel: 'Chat22',
+              title: 'Chat22',
+              drawerIcon: () => (
+                <MIcon name="chatbubbles" size={20} color="gray" />
+              ),
+            }}>
+            {() => (
+              <Tab.Navigator>
+                <Tab.Screen
+                  name="Chat-tab"
+                  component={ChatScreen}
+                  options={{
+                    headerShown: false,
+                    tabBarLabel: 'Chats',
+                    tabBarIcon: ({color, size}) => (
+                      <MIcon name="chatbubbles" size={size} color={color} />
+                    ),
+                  }}
+                />
+                <Tab.Screen
+                  name="Chat-tabss"
+                  component={HomeScreen}
+                  options={{
+                    headerShown: false,
+                    tabBarLabel: 'Chatsss',
+                    tabBarIcon: ({color, size}) => (
+                      <MIcon name="chatbubbles" size={size} color={color} />
+                    ),
+                  }}
+                />
+              </Tab.Navigator>
+            )}
+          </Drawer.Screen>
         </Drawer.Navigator>
       ) : (
         <Stack.Navigator>
--- src/screens/homseScreen.tsx
+++ src/screens/homseScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -6,9 +6,13 @@ import {
   ScrollView,
   StyleSheet,
   TextInput,
+  KeyboardAvoidingView,
+  RefreshControl,
 } from 'react-native';
 import {DrawerActions} from '@react-navigation/native';
 import {useAuth} from '../hooks/authContext';
+import MIcon from 'react-native-vector-icons/Ionicons';
+import MIcon1 from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = ({navigation}: {navigation: any}) => {
   const {logout} = useAuth();
@@ -20,122 +24,80 @@ const HomeScreen = ({navigation}: {navigation: any}) => {
       console.error('Error occurred during logout:', error);
     }
   };
+  const [Refreshing, setRefreshing] = useState(false);
 
   const handleSearch = () => {
     // You can perform search logic here if needed
     console.log('Searching for:');
   };
 
+  const [Items, setItems] = useState([
+    {key: 1, sender: 'Me', message: "Hey, how's it going?"},
+    {key: 2, sender: 'layl', message: "Hey, how's it going?"},
+    {key: 3, sender: 'Bryan', message: "Hey, how's it going?"},
+    {key: 4, sender: 'Karl', message: "Hey, how's it going?"},
+    {key: 5, sender: 'Clarence', message: "Hey, how's it going?"},
+    {key: 6, sender: 'Nina', message: "Hey, how's it going?"},
+    {key: 7, sender: 'Paulo', message: "Hey, how's it going?"},
+    {key: 8, sender: 'Me', message: "Hey, how's it going?"},
+    {key: 9, sender: 'layl', message: "Hey, how's it going?"},
+    {key: 10, sender: 'Bryan', message: "Hey, how's it going?"},
+    {key: 11, sender: 'Karl', message: "Hey, how's it going?"},
+    {key: 12, sender: 'Clarence', message: "Hey, how's it going?"},
+    {key: 13, sender: 'Nina', message: "Hey, how's it going?"},
+    {key: 14, sender: 'Paulo', message: "Hey, how's it going?"},
+  ]);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setItems([...Items, {key: 69, sender: 'test', message: 'test message'}]);
+      setRefreshing(false);
+    }, 2000);
+  };
+
   return (
-    <View style={{flex: 1}}>
-      <Text>Hello HomeScreen</Text>
+    <View className="h-full">
+      {/* <Text>Hello HomeScreen</Text>
       <TouchableOpacity
         onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
         <Text>Open Drawer</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={logoutUser}>
         <Text className="text-blue-500"> Logout</Text>
-      </TouchableOpacity>
-      <View className="flex-row items-center py-3 px-4 bg-gray-200">
+      </TouchableOpacity> */}
+      <View className="flex-row items-center py-3 px-4">
         <TextInput
           className="flex-1 p-2 bg-white rounded-md"
           placeholder="Search..."
         />
       </View>
       <ScrollView
-        className="grow bg-slate-300"
+        className="flex-1"
+        refreshControl={
+          <RefreshControl
+            refreshing={Refreshing}
+            onRefresh={onRefresh}
+            colors={['#60A3D9']}
+          />
+        }
         showsVerticalScrollIndicator={false}>
-        <View className="space-y-3 p-5">
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
-          <View className="w-full bg-gray-200 p-3 rounded-xl">
-            <Text style={styles.sender}>Me:</Text>
-            <Text style={styles.message}>Hey, how's it going?</Text>
-          </View>
+        <View className="space-y-3 px-5 py-2">
+          {Items.map(item => (
+            <View className="w-full bg-gray-200 p-4 rounded-xl" key={item.key}>
+              <Text style={styles.sender}>{item.sender}:</Text>
+              <Text style={styles.message}>{item.message}</Text>
+            </View>
+          ))}
         </View>
       </ScrollView>
+      {/* <>
+        <View className="flex flex-row h-16 w-full justify-evenly items-center bg-[#60A3D9] rounded-t-3xl">
+          <MIcon name="chatbubbles" size={25} color="black" />
+          <MIcon name="people" size={25} color="black" />
+          <MIcon1 name="flash" size={25} color="black" />
+        </View>
+      </> */}
     </View>
   );
 };
